bed4d3780e7dfe9b71b2074edc5a9f15a92d11fa
build-africa-eng <afrcanfuture@gmail.com>
2025-06-20 16:09:38 +0300

    Update buildx.sh

--- buildx.sh
+++ buildx.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+
+# Login securely
+echo "$DOCKERHUB_TOKEN" | docker login -u "$DOCKERHUB_USER" --password-stdin
+
+# Build and push
 docker buildx create --name winbuilder --use || docker buildx use winbuilder
-docker buildx build --platform windows/amd64 -t windows-devbox:latest --load .
+docker buildx build --platform windows/amd64 \
+  -t $DOCKERHUB_USER/windows-devbox:latest \
+  --push .
